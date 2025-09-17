7506f116004e8c4f422550856ce992d1d3a4fa9d
Anthony Barragan <anthony.bar.89@gmail.com>
2025-09-16 22:37:57 -0700

    created api-wrapper. automatically attempts to refresh if no access token. Only does it once

--- app/(protected)/teacher/dashboard/page.tsx
+++ app/(protected)/teacher/dashboard/page.tsx
@@ -1,7 +1,40 @@
-import React from 'react'
+"use client";
+import { Button } from '@/components/ui/button'
+import apiFetch from '@/lib/api-wrapper';
+import React, { useState } from 'react'
+
+type User = {
+  id: string
+  email: string
+  username: string
+}
 
 export default function Dashboard() {
+
+  const [users, setUsers] = useState<User[]>([])
+
+  async function getUsers(): Promise<void> {
+    try {
+      const response = await apiFetch("users/allUsers")
+      console.log('response ', response)
+      const users = await response.json();
+      setUsers(users)
+    } catch (err) {
+        console.error('error getting users ', err);
+    }
+  }
+
   return (
-    <div>Dashboard</div>
+    <>
+      <Button onClick={getUsers}>
+        Get User
+      </Button>
+
+      {users && users.length > 0 && users.map((user: User) => (
+        <div key={user.id}>
+          <p>{user.email}</p>
+        </div>
+      ))}
+    </>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,41 @@
+"use client";
+import { Button } from '@/components/ui/button'
+import apiFetch from '@/lib/api-wrapper';
+import React, { useState } from 'react'
 
-export default function Home() {
+type User = {
+  id: string
+  email: string
+  username: string
+}
+
+export default function Dashboard() {
+
+  const [users, setUsers] = useState<User[]>([])
+
+  async function getUsers(): Promise<void> {
+    try {
+      const response = await apiFetch("users/allUsers")
+      console.log('response ', response)
+      const users = await response.json();
+      setUsers(users)
+    } catch (err) {
+        console.error('error getting users ', err);
+    }
+  }
+
   return (
-    <main className="min-h-screen wrapper bg-gradient-to-br p-6">
-      <p className="jumbotron text-center mt-10">This is the Jumbotron</p>
-    </main>
-  );
+    <>
+      <Button onClick={getUsers}>
+        Get User
+      </Button>
+      <p>layout</p>
+
+      {users && users.length > 0 && users.map((user: User) => (
+        <div key={user.id}>
+          <p>{user.email}</p>
+        </div>
+      ))}
+    </>
+  )
 }
