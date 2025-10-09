ab255286e4aae28b8a25d0ae083967b6f2ca873f
Anthony Barragan <anthony.bar.89@gmail.com>
2025-10-08 22:11:06 -0700

    added manifest.ts fro basic pwa icon and colors

---
+++ app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+    return {
+        name: 'Teach For Free',
+        short_name: 'TFF',
+        description: 'Teacher Scheduler',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#0F0F19',
+        theme_color: '#A197F8',
+        icons: [
+            {
+                src: '/images/pwaImg.png',
+                sizes: '192x192',
+                type: 'image/png',
+            },
+            {
+                src: '/images/pwaImg.png',
+                sizes: '512x512',
+                type: 'image/png',
+            },
+        ],
+    }
+}
