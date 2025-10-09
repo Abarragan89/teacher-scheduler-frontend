13a9c024d45f3c9d91f3816d15aab648b284dad0
Anthony Barragan <anthony.bar.89@gmail.com>
2025-10-09 07:55:23 -0700

    added PWA installation and boilerplate for push notifications

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import CalendarMonth from '@/components/shared/calendar-month';
+import PushNotificationManager from '@/components/push-notifications';
+import InstallPrompt from '@/components/install-prompt';
 
 export default async function Dashboard() {
 
   return (
     <main className='wrapper'>
-        <CalendarMonth />
+      <CalendarMonth />
+      <div className="mt-8 space-y-4">
+        <InstallPrompt />
+        <PushNotificationManager />
+      </div>
     </main>
   )
 }
