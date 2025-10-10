965180af493932b7cbd6c05b2151b45ad62529f2
Anthony Barragan <anthony.bar.89@gmail.com>
2025-10-09 20:52:31 -0700

    making an attempt to get push notifications

--- lib/api/services/pushNotifications/server.ts
+++ lib/api/services/pushNotifications/server.ts
@@ -15,10 +15,13 @@ export const serverPushNotifications = {
     },
 
     async unsubscribe(endpoint: string) {
+        console.log('🛑 Unsubscribing from notifications for endpoint:', endpoint)
         const response = await serverFetch('/api/notifications/unsubscribe', {
             method: 'DELETE',
             body: JSON.stringify({ endpoint }),
         });
+        console.log(response)
+        console.log(await response.json())
         if (!response.ok) throw new Error('Failed to unsubscribe from notifications');
         return response.json();
     },
