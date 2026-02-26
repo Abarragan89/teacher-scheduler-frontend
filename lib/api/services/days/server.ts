4e748eac5c4edabac1dbc1d21181d31b07fe8747
Anthony Barragan <anthony.bar.89@gmail.com>
2026-02-26 08:16:56 -0800

    added console.logs for fetchign single day

--- lib/api/services/days/server.ts
+++ lib/api/services/days/server.ts
@@ -11,9 +11,11 @@ export const serverDays = {
     },
 
     async findSingleDay(dayId: string) {
+        console.log('Finding single day with ID:', dayId);
         const response = await serverFetch(`/days/single-day/${dayId}`, {
             method: 'GET',
         });
+        console.log('Response status:', response.status);
         if (!response.ok) throw new Error('Failed to find day');
         return response.json();
     }
